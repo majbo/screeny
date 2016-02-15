e7b4e5e3fc3addb1145253679f9a773dec092f76
majbo <codingmajbo@gmail.com>
2016-02-15 20:54:37 +0100

    Initial commit
    So far this is only a prototype...

---
+++ Screeny/FullscreenImage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Screeny
+{
+    /// <summary>
+    /// Interaction logic for FullscreenImage.xaml
+    /// </summary>
+    public partial class FullscreenImage : Window
+    {
+        public FullscreenImage(ImageSource source)
+        {
+            InitializeComponent();
+            image.ImageSource = source;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.Close();
+            }
+        }
+
+
+        private void surface_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //http://csharphelper.com/blog/2014/12/let-user-move-resize-rectangle-wpf-c/
+        }
+    }
+}
---
+++ Screeny/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Screeny
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void captureScreen_Click(object sender, RoutedEventArgs e)
+        {
+            string test = @"c:\temp\lll.jpg";
+            ScreenCapturer.CaptureAndSave(test, CaptureMode.Screen, ImageFormat.Bmp);
+
+            var bitmap = new BitmapImage(new Uri(test));
+
+            var fullscreenImage = new FullscreenImage(bitmap);
+            fullscreenImage.Show();
+
+        }
+    }
+}
